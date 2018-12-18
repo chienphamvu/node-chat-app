73041fa3d0401531f4d24832d17a4bc7593774e7
Chien Pham <chienphamvu@gmail.com>
2018-12-18 17:30:45 +0700

    Add Users management

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const socketIO = require('socket.io')
 
 
 const {generateMessage, generateLocationMessage} = require('./utils/message')
+const {isRealString} = require('./utils/validation.js')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 var app = express()
@@ -16,9 +17,18 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected')
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+    socket.on('join', (params, callback) => {
+        if (!isRealString(params.name) || !isRealString(params.room)) {
+            callback('Name and room name are required')
+        }
 
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user joined'))
+        socket.join(params.room)
+
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined!`))
+
+        callback()
+    })
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message)
