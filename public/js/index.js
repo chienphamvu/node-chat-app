5ef9117e190a906b204821396854b914cb177297
Chien Pham <chienphamvu@gmail.com>
2018-12-18 14:08:37 +0700

    Implement Mustachejs

--- public/js/index.js
+++ public/js/index.js
@@ -11,29 +11,35 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a')
-    var li = jQuery('<li></li>')
-    li.text(`${message.from} ${formattedTime}: ${message.text}`)
 
-    jQuery('#messages').append(li)
+    var template = jQuery('#message-template').html()
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    })
+
+    jQuery('#messages').append(html)
 })
 
 socket.emit('createMessage', {
     from: 'Frank',
     text: 'Hi'
 }, function() {
-    conlose.log('Got it')
+    console.log('Got it')
 })
 
 socket.on('newLocationMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a')
-    var li = jQuery('<li></li>')
-    var a = jQuery('<a target="_blank">My current location</a>')
 
-    li.text(`${message.from} ${formattedTime}: `)
-    a.attr('href', message.url)
+    var template = jQuery('#location-message-template')
+    var html = Mustache.render(template, {
+        from: message.from,
+        createdAt: formattedTime,
+        url: message.url
+    })
 
-    li.append(a)
-    jQuery('#message').append(li)
+    jQuery('#message').append(html)
 })
 
 jQuery('#message-form').on('submit', function(e) {
