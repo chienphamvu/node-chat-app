3944aaf1786c62122bdccefc122f86610a5e8a48
Chien Pham <chienphamvu@gmail.com>
2018-12-18 13:50:31 +0700

    Add timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -10,43 +10,61 @@ socket.on('disconnect', function() {
 })
 
 socket.on('newMessage', function(message) {
-    console.log('newMessage', message)
+    var formattedTime = moment(message.createdAt).format('h:mm a')
     var li = jQuery('<li></li>')
-    li.text(`${message.from}: ${message.text}`)
+    li.text(`${message.from} ${formattedTime}: ${message.text}`)
 
     jQuery('#messages').append(li)
 })
 
 socket.emit('createMessage', {
-	from: 'Frank',
-	text: 'Hi'
-}, function(data) {
-	conlose.log('Got it', data)
+    from: 'Frank',
+    text: 'Hi'
+}, function() {
+    conlose.log('Got it')
+})
+
+socket.on('newLocationMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a')
+    var li = jQuery('<li></li>')
+    var a = jQuery('<a target="_blank">My current location</a>')
+
+    li.text(`${message.from} ${formattedTime}: `)
+    a.attr('href', message.url)
+
+    li.append(a)
+    jQuery('#message').append(li)
 })
 
 jQuery('#message-form').on('submit', function(e) {
-	e.preventDefault()
+    e.preventDefault()
 
-	socket.emit('createMessage', {
-		from: 'User',
-		text: jQuery('[name=message]').val()
-	}, function() {
+    var messageTextbox = jQuery('[name=message]')
 
-	})
+    socket.emit('createMessage', {
+        from: 'User',
+        text: messageTextbox.val()
+    }, function() {
+        messageTextbox.val('')
+    })
 })
 
 var locationButton = jQuery('#send-location')
 locationButton.on('click', function() {
-	if (!navigator.geolocation) {
-		return alert('Geolocation not supported by your browser')
-	}
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser')
+    }
 
-	navigator.geolocation.getCurrentPosition(function(position) {
-		socket.emit('createLocationMessage', {
-			latitude: position.coords.latitude,
-			longtitude: position.coords.longitude
-		})
-	}, function() {
-		alert('Unable to fetch location')
-	})
+    locationButton.attr('disabled', 'disabled').text('Sending Location...')
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send Location')
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longtitude: position.coords.longitude
+        })
+    }, function() {
+        locationButton.removeAttr('disabled')
+        alert('Unable to fetch location').text('Send Location')
+    })
 })
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const socketIO = require('socket.io')
 
 
-const {generateMessage} = require('./utils/message')
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 var app = express()
@@ -23,11 +23,11 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message)
         io.emit('newMessage', generateMessage(message.from, message.text))
-        callback('Some data goes here')
+        callback()
     })
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`))
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
     })
 
     socket.on('disconnect', () => {
